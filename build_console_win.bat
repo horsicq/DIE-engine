b855d4ea5e5f92af1183a311311f1cff16d5be78
horsicq <horsicq@gmail.com>
2014-10-14 11:48:58 +0200

    Update

--- build_console_win.bat
+++ build_console_win.bat
@@ -3,8 +3,8 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
 set JOM="C:\Qt\jom\jom.exe"
 set QMAKE="C:\Qt\4.8.6\bin\qmake.exe"
 
-cd DIE_source/dielib/
-%QMAKE% dielib.pro -spec win32-msvc2010
+cd DIE_source/console/
+%QMAKE% console.pro -spec win32-msvc2010
 %JOM% -j 8 -f Makefile.Release clean
 %JOM% -j 8 -f Makefile.Release
 cd ../../
@@ -12,4 +12,3 @@ cd ../../
 del Makefile
 del Makefile.Release
 del Makefile.Debug
-
