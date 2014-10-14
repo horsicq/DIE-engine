5f3b62da6bfb8a2874a8e771af55ef9afb2cd645
horsicq <horsicq@gmail.com>
2014-10-14 10:46:31 +0200

    Update file

--- build_die_win.bat
+++ build_die_win.bat
@@ -1,8 +1,12 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
 
 set JOM="C:\Qt\jom\jom.exe"
 set QMAKE="C:\Qt\4.8.6\bin\qmake.exe"
 
 %QMAKE% DIE_source/dielib/dielib.pro -spec win32-msvc2010
-%JOM% -j 8 -f DIE_source/dielib/Makefile.Release clean
-%JOM% -j 8 -f DIE_source/dielib/Makefile.Release
+%JOM% -j 8 -f Makefile.Release clean
+%JOM% -j 8 -f Makefile.Release
+
+del Makefile
+del Makefile.Release
+del Makefile.Debug
