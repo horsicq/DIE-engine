98e99140fa9f380f8252761c205979366090b5ae
horsicq <horsicq@gmail.com>
2014-10-14 11:57:47 +0200

    Update

--- build_diedll_win.bat
+++ build_diedll_win.bat
@@ -3,11 +3,9 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
 set JOM="C:\Qt\jom\jom.exe"
 set QMAKE="C:\Qt\4.8.6\bin\qmake.exe"
 
-cd DIE_source/diedll/
-%QMAKE% diedll.pro -spec win32-msvc2010
+%QMAKE% DIE_source/diedll/diedll.pro -spec win32-msvc2010
 %JOM% -j 8 -f Makefile.Release clean
 %JOM% -j 8 -f Makefile.Release
-cd ../../
 
 del Makefile
 del Makefile.Release
