2e2a102879bedb70a57fa96222e672f539d323ee
horsicq <horsicq@gmail.com>
2019-06-29 01:29:33 +0200

    Fix

--- build_console_mac.sh
+++
@@ -1,1 +0,0 @@
-#!/bin/sh -xqmake DIE_source/console_source/console_source.pro -r -spec macx-g++ CONFIG+=release CONFIG+=x86_64make cleanmakerm -f Makefilefind . -type f -name "*.o" -exec rm -f {} \;find . -type f -name "moc_*" -exec rm -f {} \;
