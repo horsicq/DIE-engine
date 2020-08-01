17b7b3f9db92e4005b04ffbb031de140c9755bff
horsicq <horsicq@gmail.com>
2020-08-01 11:24:26 +0200

    Fix

--- build_winxp.bat
+++ build_winxp.bat
@@ -2,7 +2,7 @@ set VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
 set SEVENZIP_PATH="C:\Program Files\7-Zip"
 set QT_PATH=C:\Qt5.6.3\5.6.3\msvc2013
 
-set BUILD_NAME=xpeviewer_winxp_portable
+set BUILD_NAME=die_winxp_portable
 set SOURCE_PATH=%~dp0
 mkdir %SOURCE_PATH%\build
 mkdir %SOURCE_PATH%\build\loader
