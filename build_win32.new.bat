bf975dc5357d91e49c9e8f10236b5d654bab0506
horsicq <horsicq@gmail.com>
2020-11-16 17:27:23 +0100

    Fix

--- build_win32.new.bat
+++ build_win32.new.bat
@@ -9,7 +9,7 @@ mkdir %SOURCE_PATH%\release
 set /p RELEASE_VERSION=<%SOURCE_PATH%\release_version.txt
 
 set QT_PATH=%QT_PATH%
-call %VS_PATH%\VC\Auxiliary\Build\vcvars64.bat
+call %VS_PATH%\VC\Auxiliary\Build\vcvars32.bat
 set GUIEXE=die.exe
 set CONEXE=diec.exe
 set ZIP_NAME=%BUILD_NAME%_%RELEASE_VERSION%
