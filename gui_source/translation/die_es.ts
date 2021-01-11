55ad61d047d66f84f260b2e0202ae9b583fe8f11
horsicq <horsicq@gmail.com>
2021-01-11 01:23:24 +0100

    Fix

--- gui_source/translation/die_es.ts
+++ gui_source/translation/die_es.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS>
+<TS version="2.1" language="es_ES">
     <context>
         <name>Archive_widget</name>
         <message>
--- gui_source/translation/die_ko.ts
+++ gui_source/translation/die_ko.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS>
+<TS version="2.1" language="ko_KR">
     <context>
         <name>Archive_widget</name>
         <message>
