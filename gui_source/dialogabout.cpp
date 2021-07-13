7c319ee2a00b74a8a4b32eabcec5ffb67c88ca2e
horsicq <horsicq@gmail.com>
2021-07-13 21:39:01 +0200

    Fix: 2021-07-13

--- gui_source/dialogabout.cpp
+++ gui_source/dialogabout.cpp
@@ -90,6 +90,7 @@ USERINFO _userinfo_records[]=
     {QString("FrenchYeti"),             QString("FrenchYeti")},
     {QString("Nông Hoàng Tú"),          QString("NongHoangTu")},
     {QString("Arnaud Dovi(class101)"),  QString("ArnaudDovi")},
+    {QString("Javi VR"),                QString("JaviVR")},
     {QString("Adam(Hexacorn)"),         QString("Hexacorn")},
 };
 
