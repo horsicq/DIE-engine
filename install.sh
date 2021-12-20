e3c649d35091f577e982665fdb24031144bcbdab
horsicq <horsicq@gmail.com>
2021-12-20 08:36:32 +0100

    Fix: 2021-12-20

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ export X_SOURCE_PATH=$PWD
 
 cp -f $X_SOURCE_PATH/build/release/die                              /usr/bin/
 cp -f $X_SOURCE_PATH/build/release/diec                             /usr/bin/
-cp -f $X_SOURCE_PATH/DEBIAN/die.desktop                             /usr/share/applications/
-cp -Rf $X_SOURCE_PATH/DEBIAN/hicolor/                               /usr/share/icons/
+cp -f $X_SOURCE_PATH/LINUX/die.desktop                             /usr/share/applications/
+cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                               /usr/share/icons/
 cp -Rf $X_SOURCE_PATH/XStyles/qss/                                  /usr/lib/die/
 cp -Rf $X_SOURCE_PATH/Detect-It-Easy/info/                          /usr/lib/die/
 cp -Rf $X_SOURCE_PATH/Detect-It-Easy/db/                            /usr/lib/die/
