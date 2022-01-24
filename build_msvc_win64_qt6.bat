971a0828be5c4f906a35c92ed3c7921f29a17d4d
horsicq <horsicq@gmail.com>
2022-01-24 18:43:14 +0100

    Fix: 2022-01-24

---
+++ build_msvc_win64_qt6.bat
@@ -0,0 +1,48 @@
+set VSVARS_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+set QMAKE_PATH="C:\Qt\6.2.2\msvc2019_64\bin\qmake.exe"
+set SEVENZIP_PATH="C:\Program Files\7-Zip\7z.exe"
+set INNOSETUP_PATH="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
+
+set X_SOURCE_PATH=%~dp0
+set X_BUILD_NAME=die
+set X_BUILD_PREFIX=win64_qt6
+set /p X_RELEASE_VERSION=<%X_SOURCE_PATH%\release_version.txt
+
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_init
+
+IF NOT [%X_ERROR%] == [] goto exit
+
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_build %X_SOURCE_PATH%\die_source.pro
+
+cd %X_SOURCE_PATH%\gui_source
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_translate gui_source_tr.pro 
+cd %X_SOURCE_PATH%
+
+call %X_SOURCE_PATH%\build_tools\windows.cmd check_file %X_SOURCE_PATH%\build\release\die.exe
+
+IF NOT [%X_ERROR%] == [] goto exit
+
+call %X_SOURCE_PATH%\build_tools\windows.cmd check_file %X_SOURCE_PATH%\build\release\diec.exe
+
+IF NOT [%X_ERROR%] == [] goto exit
+
+call %X_SOURCE_PATH%\build_tools\windows.cmd check_file %X_SOURCE_PATH%\build\release\diel.exe
+
+IF NOT [%X_ERROR%] == [] goto exit
+
+mkdir %X_SOURCE_PATH%\release\%X_BUILD_NAME%\signatures
+
+copy %X_SOURCE_PATH%\build\release\die.exe %X_SOURCE_PATH%\release\%X_BUILD_NAME%\
+copy %X_SOURCE_PATH%\build\release\diec.exe %X_SOURCE_PATH%\release\%X_BUILD_NAME%\
+copy %X_SOURCE_PATH%\build\release\diel.exe %X_SOURCE_PATH%\release\%X_BUILD_NAME%\
+xcopy %X_SOURCE_PATH%\XStyles\qss %X_SOURCE_PATH%\release\%X_BUILD_NAME%\qss /E /I
+xcopy %X_SOURCE_PATH%\Detect-It-Easy\db %X_SOURCE_PATH%\release\%X_BUILD_NAME%\db /E /I
+xcopy %X_SOURCE_PATH%\Detect-It-Easy\info %X_SOURCE_PATH%\release\%X_BUILD_NAME%\info /E /I
+xcopy %X_SOURCE_PATH%\signatures\crypto.db %X_SOURCE_PATH%\release\%X_BUILD_NAME%\signatures\
+
+call %X_SOURCE_PATH%\build_tools\windows.cmd deploy_qt die.exe
+
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_release
+
+:exit
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_clear
