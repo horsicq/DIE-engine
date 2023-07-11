ce1a722ad4dee88f187eef003f2ed6aa2d4838ff
horsicq <horsicq@gmail.com>
2023-07-11 18:37:06 +0200

    Fix: 2023-07-11

---
+++ gui_source/translation/die_fa.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fa_IR">
+<context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="../dialogabout.ui" line="17"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogabout.ui" line="51"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogabout.cpp" line="38"/>
+        <source>Bugreports</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogabout.cpp" line="42"/>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogabout.cpp" line="47"/>
+        <location filename="../dialogabout.cpp" line="52"/>
+        <source>Donate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogabout.cpp" line="57"/>
+        <source>Source code</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogOptions</name>
+    <message>
+        <location filename="../dialogoptions.ui" line="17"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.cpp" line="38"/>
+        <source>Scan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.cpp" line="41"/>
+        <source>Signatures</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.cpp" line="44"/>
+        <source>Hex</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.cpp" line="47"/>
+        <source>Disasm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.cpp" line="50"/>
+        <source>Online tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dialogoptions.cpp" line="53"/>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogSelectStyle</name>
+    <message>
+        <location filename="../dialogselectstyle.ui" line="14"/>
+        <source>Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GuiMainWindow</name>
+    <message>
+        <location filename="../guimainwindow.ui" line="21"/>
+        <source>File name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="91"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="98"/>
+        <source>Demangle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="118"/>
+        <source>Shortcuts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="125"/>
+        <source>Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="132"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.ui" line="139"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.cpp" line="272"/>
+        <source>Open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../guimainwindow.cpp" line="272"/>
+        <source>All files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LiteMainWindow</name>
+    <message>
+        <location filename="../../lite_source/litemainwindow.ui" line="23"/>
+        <source>File name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.ui" line="58"/>
+        <source>Recursive scan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.ui" line="65"/>
+        <source>Deep scan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.ui" line="72"/>
+        <source>Heuristic scan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.ui" line="79"/>
+        <source>Verbose</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.ui" line="86"/>
+        <source>All types</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.ui" line="116"/>
+        <source>Scan</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.ui" line="136"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.cpp" line="102"/>
+        <source>msec</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.cpp" line="120"/>
+        <source>Open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../lite_source/litemainwindow.cpp" line="120"/>
+        <source>All files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
