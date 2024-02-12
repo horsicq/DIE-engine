1f6eec559ed0031864ec977940369ab66c3ae04d
guihkx <626206+guihkx@users.noreply.github.com>
2024-02-12 14:31:38 -0300

    Remove optional keys from the Linux desktop file
    The "Version" key, in particular, is not the version of the app:
    
    >Version of the Desktop Entry Specification that the desktop entry
    >conforms with. Entries that confirm with this version of the
    >specification should use 1.5. Note that the version field is not
    >required to be present.
    
    https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html#:~:text=YES-,Version,-Version%20of%20the

--- create_appimage.sh
+++ create_appimage.sh
@@ -11,7 +11,6 @@ cp -f $X_SOURCE_PATH/build/release/die                              $X_SOURCE_PA
 #cp -f $X_SOURCE_PATH/build/release/diec                             $X_SOURCE_PATH/release/appDir/usr/bin/
 #cp -f $X_SOURCE_PATH/build/release/diel                             $X_SOURCE_PATH/release/appDir/usr/bin/
 cp -f $X_SOURCE_PATH/LINUX/die.desktop                              $X_SOURCE_PATH/release/appDir/usr/share/applications/
-sed -i "s/#VERSION#/1.0/"                                           $X_SOURCE_PATH/release/appDir/usr/share/applications/die.desktop
 cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                                $X_SOURCE_PATH/release/appDir/usr/share/icons/
 cp -Rf $X_SOURCE_PATH/images/                                       $X_SOURCE_PATH/release/appDir/usr/lib/die/
 cp -Rf $X_SOURCE_PATH/XStyles/qss/                                  $X_SOURCE_PATH/release/appDir/usr/lib/die/
