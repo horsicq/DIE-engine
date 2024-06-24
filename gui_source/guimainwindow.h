decb939fa225187fd802604567f6f06b4daa9ce1
horsicq <horsicq@gmail.com>
2024-06-25 00:15:31 +0200

    Update file(s): 2024-06-25

--- gui_source/guimainwindow.h
+++ gui_source/guimainwindow.h
@@ -70,7 +70,7 @@ private slots:
     void fullScreenSlot();
 
     QString getCurrentFileName();
-    void adjustView();
+    virtual void adjustView();
     void updateShortcuts();
     void adjustFile();
     void setAdvanced(bool bState);
