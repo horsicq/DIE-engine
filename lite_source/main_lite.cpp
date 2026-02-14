1ced55c61f961f27e1ed771c3793d2f9709b157d
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2026-02-14 08:15:58 +0100

    rename window variable for diel to liteWindow

--- lite_source/main_lite.cpp
+++ lite_source/main_lite.cpp
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
     }
 
     QApplication app(argc, argv);
-    LiteMainWindow w;
-    w.show();
+    LiteMainWindow liteWindow;
+    liteWindow.show();
     return app.exec();
 }
