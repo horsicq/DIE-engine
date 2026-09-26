49bc78321a8b22ae5608bf6a3ce29e8ffff9b161
DosX <admin@kay-software.ru>
2026-09-26 15:39:33 +0300

    Preserve spaces in Linux installation paths (#200)
    The generated install recipe split prefixes containing spaces before install.sh received them. The installer also split source and destination paths in mkdir and cp calls. Quote these paths while leaving the optional db_extra glob active.

--- install.sh
+++ install.sh
@@ -7,25 +7,25 @@ else
     X_PREFIX="$1"
 fi
 
-mkdir -p  $X_PREFIX/bin
-mkdir -p  $X_PREFIX/share/applications
-mkdir -p  $X_PREFIX/share/icons
-mkdir -p  $X_PREFIX/share/metainfo
-mkdir -p  $X_PREFIX/lib/die
-mkdir -p  $X_PREFIX/lib/die/signatures
+mkdir -p  "$X_PREFIX/bin"
+mkdir -p  "$X_PREFIX/share/applications"
+mkdir -p  "$X_PREFIX/share/icons"
+mkdir -p  "$X_PREFIX/share/metainfo"
+mkdir -p  "$X_PREFIX/lib/die"
+mkdir -p  "$X_PREFIX/lib/die/signatures"
 
-cp -f $X_SOURCE_PATH/build/release/die                              $X_PREFIX/bin/
-cp -f $X_SOURCE_PATH/build/release/diec                             $X_PREFIX/bin/
-cp -f $X_SOURCE_PATH/build/release/diel                             $X_PREFIX/bin/
-cp -f $X_SOURCE_PATH/LINUX/io.github.horsicq.detect-it-easy.desktop $X_PREFIX/share/applications/
-cp -Rf $X_SOURCE_PATH/LINUX/hicolor/                                $X_PREFIX/share/icons/
-cp -f $X_SOURCE_PATH/LINUX/io.github.horsicq.detect-it-easy.metainfo.xml $X_PREFIX/share/metainfo/
-cp -Rf $X_SOURCE_PATH/dep/XStyles/qss/                                  $X_PREFIX/lib/die/
-cp -f $X_SOURCE_PATH/dep/signatures/crypto.db                           $X_PREFIX/lib/die/signatures/
-cp -Rf $X_SOURCE_PATH/dep/XInfoDB/info/                                 $X_PREFIX/lib/die/
-cp -Rf $X_SOURCE_PATH/dep/Detect-It-Easy/db/                            $X_PREFIX/lib/die/
-cp -Rf $X_SOURCE_PATH/dep/XYara/yara_rules/                             $X_PREFIX/lib/die/
-cp -Rf $X_SOURCE_PATH/dep/XPEID/peid/                                  $X_PREFIX/lib/die/
-[ -d $X_SOURCE_PATH/dep/Detect-It-Easy/db_extra ] && cp -Rf $X_SOURCE_PATH/dep/Detect-It-Easy/db_extra/*/ $X_PREFIX/lib/die/db/
-cp -Rf $X_SOURCE_PATH/dep/Detect-It-Easy/db_custom/                     $X_PREFIX/lib/die/
-cp -Rf $X_SOURCE_PATH/images/                                       $X_PREFIX/lib/die/
+cp -f "$X_SOURCE_PATH/build/release/die"                              "$X_PREFIX/bin/"
+cp -f "$X_SOURCE_PATH/build/release/diec"                             "$X_PREFIX/bin/"
+cp -f "$X_SOURCE_PATH/build/release/diel"                             "$X_PREFIX/bin/"
+cp -f "$X_SOURCE_PATH/LINUX/io.github.horsicq.detect-it-easy.desktop" "$X_PREFIX/share/applications/"
+cp -Rf "$X_SOURCE_PATH/LINUX/hicolor/"                                "$X_PREFIX/share/icons/"
+cp -f "$X_SOURCE_PATH/LINUX/io.github.horsicq.detect-it-easy.metainfo.xml" "$X_PREFIX/share/metainfo/"
+cp -Rf "$X_SOURCE_PATH/dep/XStyles/qss/"                                  "$X_PREFIX/lib/die/"
+cp -f "$X_SOURCE_PATH/dep/signatures/crypto.db"                           "$X_PREFIX/lib/die/signatures/"
+cp -Rf "$X_SOURCE_PATH/dep/XInfoDB/info/"                                 "$X_PREFIX/lib/die/"
+cp -Rf "$X_SOURCE_PATH/dep/Detect-It-Easy/db/"                            "$X_PREFIX/lib/die/"
+cp -Rf "$X_SOURCE_PATH/dep/XYara/yara_rules/"                             "$X_PREFIX/lib/die/"
+cp -Rf "$X_SOURCE_PATH/dep/XPEID/peid/"                                  "$X_PREFIX/lib/die/"
+[ -d "$X_SOURCE_PATH/dep/Detect-It-Easy/db_extra" ] && cp -Rf "$X_SOURCE_PATH"/dep/Detect-It-Easy/db_extra/*/ "$X_PREFIX/lib/die/db/"
+cp -Rf "$X_SOURCE_PATH/dep/Detect-It-Easy/db_custom/"                     "$X_PREFIX/lib/die/"
+cp -Rf "$X_SOURCE_PATH/images/"                                       "$X_PREFIX/lib/die/"
